2ede126df7de3dd3eec4851f13ef32c6582aa2d1
Fangwei Jiang <emberspirit2018@gmail.com>
2023-06-24 17:45:02 +1000

    implement login logic

---
+++ index.js
@@ -0,0 +1,33 @@
+const { exec } = require('child_process');
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const cors = require('cors');
+const router = require('./router');
+const PORT = 8080;
+exec(`lsof -i tcp:${PORT} | awk '{if(NR>1)print }'`, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    if (`${stdout} === ''`) {
+    } else {
+        exec(`kill -9 ${stdout}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`the port ${PORT} is not in use now`);
+            return;
+        });
+    }
+});
+app.use(express.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(express.static('./public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.get('', (req, res) => res.render('index', { text: 'This is EJS' }));
+app.use(cors());
+app.use(router);
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
---
+++ public/js/login.js
@@ -0,0 +1,32 @@
+const loginButton = document.getElementById('login');
+const nameInput = document.getElementById('name');
+const passwordInput = document.getElementById('password');
+const message = document.getElementById('message');
+
+loginButton.addEventListener('click', async () => {
+    try {
+        const username = nameInput.value;
+        const password = passwordInput.value;
+
+        if (username === '' || password === '') {
+            message.textContent = 'Please enter name & password';
+            setTimeout(() => (message.textContent = ''), 3000);
+        } else {
+            const response = await axios.post('http://localhost:8080/login', {
+                name: username,
+                password: password,
+            });
+
+            nameInput.value = '';
+            passwordInput.value = '';
+            message.textContent = response.data.msg;
+            setTimeout(() => (message.textContent = ''), 3000);
+        }
+    } catch (error) {
+        message.textContent = error.response.data.msg;
+        setTimeout(() => (message.textContent = ''), 3000);
+        nameInput.value = '';
+        passwordInput.value = '';
+        console.error('Error fetching users:', error.response.data.msg);
+    }
+});
